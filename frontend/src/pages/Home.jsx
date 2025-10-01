a2182577d5a1b768a45670e4319752d80052b4a8
neeraj <neerajpc799@gmail.com>
2025-10-01 19:12:02 +0530

    Changes in homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import axios from "axios";
 import { toast } from "react-toastify";
 import { useEffect } from "react";
 import EditTodoModal from "../components/EditTodoModal";
